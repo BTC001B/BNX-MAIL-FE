b726361bf0c354ed0e2992586e7c813f4c7b4892
btc002b.btcglobal@gmail.com <btc002b.btcglobal@gmail.com>
2026-08-18 10:51:03 +0530

    feat: implement AppLauncher component with favorites, recents, and categorical app navigation

--- src/components/AppLauncher.jsx
+++ src/components/AppLauncher.jsx
@@ -57,15 +57,16 @@ const AppLauncher = ({ onClose, onToggleBitToolSidebar, onEdit }) => {
   return (
     <div className="flex flex-col h-full bg-white w-full select-none">
       {/* Header */}
-      <div className="flex items-center justify-between pt-4 pb-2 px-6 border-b border-gray-100 shrink-0 relative">
+      <div className="flex items-center justify-between pt-2 pb-1.5 px-6 border-b border-gray-100 shrink-0 relative">
         <div
           className="text-[24px] text-[#0f53c9] font-black tracking-tighter"
           style={{
             fontFamily: "'Saira Stencil One', 'Anton', sans-serif",
             position: 'absolute',
-            top: '-10px',
+            top: '4px',
             left: '16px',
-            lineHeight: '1'
+            lineHeight: '1',
+            zIndex: 50
           }}
         >
           BE<span style={{ marginLeft: '0.5px' }}>TA</span>
