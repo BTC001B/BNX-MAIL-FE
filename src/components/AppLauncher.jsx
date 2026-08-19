45006b35b64f7887f4e31d097cbef05fb7c0357d
btc002b.btcglobal@gmail.com <btc002b.btcglobal@gmail.com>
2026-08-19 16:47:21 +0530

    feat: implement AppLauncher component with favorites, recent apps, and categorization tabs

--- src/components/AppLauncher.jsx
+++ src/components/AppLauncher.jsx
@@ -79,7 +79,7 @@ const AppLauncher = ({ onClose, onToggleBitToolSidebar, onEdit }) => {
             marginLeft: '-10px'
           }}
         >
-          BE<span style={{ marginLeft: '0.5px' }}>TA</span>
+          BE<span style={{ marginLeft: '1px' }}>TA</span>
         </div>
 
         {/* Right buttons */}
