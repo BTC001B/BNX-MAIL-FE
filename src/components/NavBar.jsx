b56aee30215ce7e17b1f472e792298bb642b2e6b
btc002b.btcglobal@gmail.com <btc002b.btcglobal@gmail.com>
2026-08-20 09:53:43 +0530

    feat: create responsive NavBar component with navigation, profile management, and theme toggling

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -367,7 +367,7 @@ const NavBar = ({ searchQuery, setSearchQuery, onOpenMenu, onToggleDesktopSideba
           </div>
 
 
-          <div className="h-6 w-[1px] bg-gray-200 dark:bg-gray-700 ml-8 mr-0 hidden sm:block" />
+          <div className="h-6 w-[1px] bg-gray-200 dark:bg-gray-700 ml-12 mr-[-6px] hidden sm:block" />
 
           <button
             onClick={onToggleBitToolSidebar}
